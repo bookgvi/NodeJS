4512ab88e24ffdcb8311947eca8d258ca45615c7
book <bookgvi@gmail.com>
2020-01-05 23:17:11 +0300

    События

---
+++ src/events.js
@@ -0,0 +1,38 @@
+const Events = require('events');
+
+const Event = function() {}
+Event.prototype = Object.create(new Events(), {
+  constructor: {
+    value: Event,
+    enumerable: false
+  }
+})
+let eventName = 'greeting'
+Event.prototype.sayHi = function (val) {
+  return this.emit(eventName, val)
+}
+
+const user = new Event()
+
+user.on(eventName, function (val) {
+  console.log('From event to U:', val, this)
+})
+
+console.dir(user)
+user.sayHi('QQQ')
+
+/*
+*
+* Checked some case
+* */
+eventName = 'request'
+const newEvent = new Events ()
+
+newEvent.on(eventName, function (request) {
+  request.approved = true
+})
+newEvent.on(eventName, function (request) {
+  console.log(request)
+})
+
+newEvent.emit(eventName, { a: 1 })
