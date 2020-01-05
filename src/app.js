89fa094380ff233d066584c2e422491b00971559
book <bookgvi@gmail.com>
2020-01-05 19:36:13 +0300

    required('os');

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+const os = require('os').cpus()
+
 const len = 5
 let arg = []
 
@@ -6,3 +8,5 @@ arg = Array.apply(null, { length: len }).map((item, index) => process.argv[index
 arg.forEach((item, index) => {
   console.log(`Argument arg${index} = ${arg[index]}`)
 })
+
+console.log('os cpus() - ', os.length)
