1a2cddca62405828cd8613e5cd6b32398219b0c0
book <bookgvi@gmail.com>
2020-01-14 00:23:30 +0300

    Middleware for 404 status. Page error404

--- express&vue/backend/app.js
+++ express&vue/backend/app.js
@@ -4,14 +4,20 @@ const fs = require('fs');
 const host = 'localhost';
 const port = 7000;
 const statusOK = 200;
+const statusNotFound = 404;
 
 const server = express();
-
 server.get('/', (req, res) => {
-  res.status = statusOK;
+  res.statusCode = statusOK;
   res.header('Content-type', 'text/html; charset=utf-8;')
   const readStream = fs.createReadStream('../frontend/index.html', 'utf-8')
   readStream.pipe(res)
+})
+server.use((req, res, next) => {
+  res.statusCode = statusNotFound;
+  res.header('Content-type', 'text/html; charset=utf-8;')
+  const readStream = fs.createReadStream('../frontend/error404.html', 'utf-8');
+  readStream.pipe(res);
 })
 
 server.listen(port, host, () => {
